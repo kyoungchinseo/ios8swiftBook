19cde3731b071cfb2ec397b5c513581ddd013e8c
kyoungchin <kcseo.acm@gmail.com>
2015-07-16 22:30:28 +0900

    make UILabel using UIKit in Swift

---
+++ project06-swift-uikit.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+let myLabel = UILabel(frame: CGRectMake(0, 0, 200, 50))
+myLabel.backgroundColor = UIColor.redColor()
+myLabel.text = "Swift is great!"
+myLabel.textAlignment = .Center
+myLabel.font = UIFont(name: "Georgia", size: 24)
+myLabel
+
